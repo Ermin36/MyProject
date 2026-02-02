ea5022dd8a55b860065934ac02af5468106460e3
Рикки <erikgerashchenko@mail.ru>
2026-02-02 08:46:29 +0300

    Создан processing.py и test_processing.py

---
+++ tests/test_processing.py
@@ -0,0 +1,54 @@
+import pytest
+
+from utils import filter_by_state, sort_by_date
+
+
+# functions
+@pytest.fixture
+def list_for_test() -> list[dict[str, str]]:
+
+    return [
+        {"id": "434235", "count": "3"},
+        {"state": "EXECUTED", "id": "15", "date": "15.03.24"},
+        {"state": "Cancel", "id": "42", "date": "11.03.24"},
+        {"state": "EXECUTED", "id": "15", "date": "05.03.24"},
+        {"state": "NoN", "id": "15", "date": "16.03.24"},
+        {"state": "EXECUTED", "id": "15", "date": "14.03.24"},
+    ]
+
+
+class TestFilterByState:
+
+    def test_filter_by_executed(self, list_for_test: list[dict[str, str]]) -> None:
+
+        result = filter_by_state(list_for_test)
+        assert all(item["state"] == "EXECUTED" for item in result)
+        assert len(result) == 3
+
+    def test_filter_by_non(self, list_for_test: list[dict[str, str]]) -> None:
+
+        result = filter_by_state(list_for_test, "NoN")
+        assert len(result) == 1
+        assert result[0]["state"] == "NoN"
+
+    def test_filter_by_empty_list(self, list_for_test: list[dict[str, str]]) -> None:
+
+        result = filter_by_state([])
+        assert result == []
+
+
+class TestSortByDate:
+
+    def test_sort_by_date_desc(self, list_for_test: list[dict[str, str]]) -> None:
+
+        result = sort_by_date(list_for_test)
+        dates = [item.get("date", "0") for item in result]
+
+        assert dates == ["16.03.24", "15.03.24", "14.03.24", "11.03.24", "05.03.24", "0"]
+
+    def test_sort_by_date_asc(self, list_for_test: list[dict[str, str]]) -> None:
+
+        result = sort_by_date(list_for_test, False)
+        dates = [item.get("date", "0") for item in result]
+
+        assert dates == ["16.03.24", "15.03.24", "14.03.24", "11.03.24", "05.03.24", "0"][::-1]
